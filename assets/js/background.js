d63ba23c7540a0705fc686ffdd1a5bbc8f668cc9
ahmetyesil <ahmet.koyuyesil@gmail.com>
2018-08-14 19:51:09 +0300

    ADD project

---
+++ assets/js/background.js
@@ -0,0 +1,22 @@
+chrome.tabs.onActiveChanged.addListener(function (tab_id) {
+    chrome.tabs.getSelected(null, function (tab) {
+        checkTab(tab);
+    });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete') {
+        checkTab(tab);
+    }
+});
+
+function checkTab(tab) {
+    const regex = /(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/;
+    if (regex.test(tab.url)) {
+        const matches = regex.exec(tab.url);
+        const id = matches[5];
+        if (id === 'DKZlHoWPGDY') {
+            alert('helal dogru video');
+        }
+    }
+}
---
+++ assets/js/custom.js
@@ -0,0 +1,86 @@
+(function () {
+
+    let PAGE_LOGIN = '#yt-login';
+    let PAGE_REGISTER = '#yt-register';
+    let PAGE_FORGOT_PASSWORD = '#yt-forgot_password';
+    let PAGE_INDEX = '#yt-index';
+
+    let PAYMENT_URL = 'https://www.ytwatch.com/payment';
+    let TOS_URL = 'https://www.ytwatch.com/tos';
+    let FAQ_URL = 'https://www.ytwatch.com/faq';
+
+
+    function pageRouting() {
+        let current_page = '#yt-login';
+        $(current_page).fadeIn();
+        $('a[href]').click(function () {
+            let t = $(this);
+            let s = t.attr('href');
+            pageRedirectByUrl(s);
+        })
+    }
+    function pageRedirectByUrl(id){
+        if(id.indexOf('http') === -1)
+        {
+            $('.yt-pages').css('display','none');
+            $(id).fadeIn();
+        }
+    }
+    function locationHref(url){
+        chrome.tabs.update(null, {
+            url:url
+        });
+    }
+    pageRouting();
+
+
+    $('#button-login').click(function () {
+        pageRedirectByUrl(PAGE_INDEX);
+    });
+    $('#link-logout').click(function () {
+        pageRedirectByUrl(PAGE_LOGIN);
+    });
+    $('#link-channel').click(function () {
+        locationHref('https://www.youtube.com/channel/UCve_taYp1VAd_WWg0lDMNlg?view_as=subscriber');
+    })
+    $('#button-register').click(function () {
+        pageRedirectByUrl(PAGE_INDEX);
+    });
+    $('#button-new-password').click(function () {
+
+    });
+
+    $('#link-payment').click(function () {
+        locationHref(PAYMENT_URL);
+    })
+    $('#link-tos').click(function () {
+        locationHref(TOS_URL);
+    })
+    $('#link-faq').click(function () {
+        locationHref(FAQ_URL);
+    })
+
+
+
+
+
+
+
+    $('#button-watch-subs-like').click(function () {
+        locationHref('https://www.youtube.com/watch?v=KklDe7HnbTY&list=PLs8GsV2H1tV8E9B6QzptvUwHXAN6sUxRI');
+    });
+    $('#button-confirm').click(function () {
+        alert('confirm event')
+    });
+    $('#button-skip').click(function () {
+        alert('skip event')
+    });
+
+
+
+
+
+
+
+
+})();
